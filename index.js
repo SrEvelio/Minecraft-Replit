a9bb00e2107ca07beb84a25bb6935451a1b1b2c0
SrEvelio <evelkiller2018@gmail.com>
2022-04-13 12:37:10 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,31 +1,11 @@
-
 const got = require('got');
 const fs = require("fs");
 const { execSync } = require("child_process");
-const path = "./paper-1.8.8-445.jar";
+const path = "./Minecraft/paper-1.8.8-445.jar";
 const nngrok = "./ngrok-stable-linux-amd64.tgz"
 
-const start = () => {
-    if (fs.existsSync(path))
-        execSync("java -jar paper-1.8.8-445.jar", {
-            stdio: "inherit"
-        });
-    else
-        console.log("\x1b[31m%s\x1b[0m", "paper-1.8.8-445.jar not found!");
-    console.log("\x1b[34m%s\x1b[0m", "I will try to download one for you.");
-    console.log("\x1b[34m%s\x1b[0m", "If for whatever reason it says that the Jar file is corrupt, you can delete the jar file and try running this again.");
-    got.stream("https://papermc.io/api/v2/projects/paper/versions/1.8.8/builds/445/downloads/paper-1.8.8-445.jar")
-        .pipe(fs.createWriteStream(path))
-        .on("finish", () => {
-            console.log("\x1b[32m%s\x1b[0m", "paper-1.8.8-445.jar downloaded!");
-            execSync("java -jar paper-1.8.8-445.jar", {
-                stdio: "inherit"
-            });
-        });
-}
-
 const ngrok = () => {
-    if (fs.existsSync(nngrok)) return;
+    if (fs.existsSync(nngrok)) return console.log("\x1b[32m%s\x1b[0m", "Ngrok already installed!");
     else
         console.log("\x1b[31m%s\x1b[0m", "Ngrok not found!")
     console.log("\x1b[31m%s\x1b[0m", "Downloading one for you")
@@ -39,5 +19,28 @@ const ngrok = () => {
         });
 }
 
+const server = () => {
+    if (fs.existsSync(path))
+        execSync("cd Minecraft && java -jar paper-1.8.8-445.jar", {
+            stdio: "inherit"
+        });
+    else
+        console.log("\x1b[31m%s\x1b[0m", "paper-1.8.8-445.jar not found!");
+    console.log("\x1b[34m%s\x1b[0m", "I will try to download one for you.");
+    console.log("\x1b[34m%s\x1b[0m", "If for whatever reason it says that the Jar file is corrupt, you can delete the jar file and try running this again.");
+    
+    // Create the Minecraft folder if it doesn't exist
+    
+    if (!fs.existsSync("./Minecraft")) fs.mkdirSync("./Minecraft");
+
+    got.stream("https://papermc.io/api/v2/projects/paper/versions/1.8.8/builds/445/downloads/paper-1.8.8-445.jar")
+        .pipe(fs.createWriteStream(path))
+        .on("finish", () => {
+            console.log("\x1b[32m%s\x1b[0m", "paper-1.8.8-445.jar downloaded!");
+            execSync("cd Minecraft && java -jar paper-1.8.8-445.jar", {
+                stdio: "inherit"
+            });
+        });
+}
 ngrok();
-start();
+server();
