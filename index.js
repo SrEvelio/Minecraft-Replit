f0a7594d7c96a031ddfc3fd6641b4aa6ab1c5b57
SrEvelio <evelkiller2018@gmail.com>
2022-04-13 12:46:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,26 +5,28 @@ const path = "./Minecraft/paper-1.8.8-445.jar";
 const nngrok = "./ngrok-stable-linux-amd64.tgz"
 
 const ngrok = () => {
-    if (fs.existsSync(nngrok)) return console.log("\x1b[32m%s\x1b[0m", "Ngrok already installed!");
-    else
+    if (fs.existsSync("./ngrok")) return console.log("\x1b[32m%s\x1b[0m", "Ngrok already installed!");
+    
+    else if(!fs.existsSync("./ngrok")) {
         console.log("\x1b[31m%s\x1b[0m", "Ngrok not found!")
     console.log("\x1b[31m%s\x1b[0m", "Downloading one for you")
     got.stream("https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz")
         .pipe(fs.createWriteStream(nngrok))
         .on("finish", () => {
             console.log("\x1b[32m%s\x1b[0m", "Ngrok downloaded!");
-            execSync("tar xvzf ngrok-stable-linux-amd64.tgz", {
+            execSync("tar xvzf ngrok-stable-linux-amd64.tgz && rm ngrok-stable-linux-amd64.tgz", {
                 stdio: "inherit"
             });
         });
+    }
 }
 
 const server = () => {
-    if (fs.existsSync(path))
+    if (fs.existsSync(path)) {
         execSync("cd Minecraft && java -jar paper-1.8.8-445.jar", {
             stdio: "inherit"
         });
-    else
+    } else if (!fs.existsSync(path)) {
         console.log("\x1b[31m%s\x1b[0m", "paper-1.8.8-445.jar not found!");
     console.log("\x1b[34m%s\x1b[0m", "I will try to download one for you.");
     console.log("\x1b[34m%s\x1b[0m", "If for whatever reason it says that the Jar file is corrupt, you can delete the jar file and try running this again.");
@@ -41,6 +43,8 @@ const server = () => {
                 stdio: "inherit"
             });
         });
+    }
 }
+
 ngrok();
 server();
