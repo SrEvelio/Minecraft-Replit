10e3ecc118ede094088fb1912d0d82ce712edb26
SrEvelio <evelkiller2018@gmail.com>
2022-04-13 21:36:53 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,2 @@
-echo Downloading Java...
-curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash -s -- --skip-rc && . ~/.jabba/jabba.sh
-jabba install adopt@1.8-0
-echo Java is installed.
-java -version
 npm i
 node index
