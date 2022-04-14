66205f25e21f9d38d643657ab78fae2fcd013f7e
SrEvelio <evelkiller2018@gmail.com>
2022-04-13 20:45:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,18 +6,18 @@ const nngrok = "./ngrok-stable-linux-amd64.tgz"
 
 const ngrok = () => {
     if (fs.existsSync("./ngrok")) return console.log("\x1b[32m%s\x1b[0m", "Ngrok already installed!");
-    
-    else if(!fs.existsSync("./ngrok")) {
+
+    else if (!fs.existsSync("./ngrok")) {
         console.log("\x1b[31m%s\x1b[0m", "Ngrok not found!")
-    console.log("\x1b[31m%s\x1b[0m", "Downloading one for you")
-    got.stream("https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz")
-        .pipe(fs.createWriteStream(nngrok))
-        .on("finish", () => {
-            console.log("\x1b[32m%s\x1b[0m", "Ngrok downloaded!");
-            execSync("tar xvzf ngrok-stable-linux-amd64.tgz && rm ngrok-stable-linux-amd64.tgz", {
-                stdio: "inherit"
+        console.log("\x1b[31m%s\x1b[0m", "Downloading one for you")
+        got.stream("https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz")
+            .pipe(fs.createWriteStream(nngrok))
+            .on("finish", () => {
+                console.log("\x1b[32m%s\x1b[0m", "Ngrok downloaded!");
+                execSync("tar xvzf ngrok-stable-linux-amd64.tgz && rm ngrok-stable-linux-amd64.tgz", {
+                    stdio: "inherit"
+                });
             });
-        });
     }
 }
 
@@ -28,20 +28,22 @@ const server = () => {
         });
     } else if (!fs.existsSync(path)) {
         console.log("\x1b[31m%s\x1b[0m", "paper-1.8.8-445.jar not found!");
-    console.log("\x1b[34m%s\x1b[0m", "I will try to download one for you, If for whatever reason it says that the Jar file is corrupt, you can delete the jar file and try running this again.");
-    
-    // Create the Minecraft folder if it doesn't exist
-    
-    if (!fs.existsSync("./Minecraft")) fs.mkdirSync("./Minecraft");
+        console.log("\x1b[34m%s\x1b[0m", "I will try to download one for you.");
+        console.log("\x1b[34m%s\x1b[0m", "If for whatever reason it says that the Jar file is corrupt, you can delete the jar file and try running this again.");
 
-    got.stream("https://papermc.io/api/v2/projects/paper/versions/1.8.8/builds/445/downloads/paper-1.8.8-445.jar")
-        .pipe(fs.createWriteStream(path))
-        .on("finish", () => {
-            console.log("\x1b[32m%s\x1b[0m", "paper-1.8.8-445.jar downloaded!");
-            execSync("cd Minecraft && java -jar paper-1.8.8-445.jar", {
-                stdio: "inherit"
+
+        // Create the Minecraft folder if it doesn't exist
+
+        if (!fs.existsSync("./Minecraft")) fs.mkdirSync("./Minecraft");
+
+        got.stream("https://papermc.io/api/v2/projects/paper/versions/1.8.8/builds/445/downloads/paper-1.8.8-445.jar")
+            .pipe(fs.createWriteStream(path))
+            .on("finish", () => {
+                console.log("\x1b[32m%s\x1b[0m", "paper-1.8.8-445.jar downloaded!");
+                execSync("cd Minecraft && java -jar paper-1.8.8-445.jar", {
+                    stdio: "inherit"
+                });
             });
-        });
     }
 }
 
